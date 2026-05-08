7a6c606d92c87109e6f1146b1ee58f010e9231e2
Juan Quiroga <793435-quiro9@users.noreply.gitlab.com>
2026-05-08 17:57:07 -0300

    update version
    version update

--- VirtualWebDisplay_Parsec/Web/HtmlTemplates/TemplateVersionHelper.cs
+++ VirtualWebDisplay_Parsec/Web/HtmlTemplates/TemplateVersionHelper.cs
@@ -22,7 +22,7 @@ public static class TemplateVersionHelper
         // Formatear como "Major.Minor.Build" (ej: "1.2.3")
         _version = assemblyVersion != null
             ? $"{assemblyVersion.Major}.{assemblyVersion.Minor}.{assemblyVersion.Build}"
-            : "1.0.2";
+            : "1.0.3";
     }
 
     /// <summary>
